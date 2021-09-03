56880cf8c95d58e258752e40d5a728127dfaf12b
rajavasanthan <rajavasanthan@gmail.com>
2021-09-03 12:33:52 +0530

    login

--- src/Login.js
+++ src/Login.js
@@ -1,28 +1,47 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import './Login.css';
+import env from "./settings";
+
 
 function Login() {
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+
+    let handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            let logindata = await axios.post(`${env.api}/login`, { username, password })
+            console.log(logindata)
+            alert(logindata.data.message)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
-        <div class="wrapper fadeInDown">
-            <div id="formContent">
+        <main class="form-signin text-center">
+            <form onSubmit={handleSubmit}>
+                <img class="mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
+                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-
-                <div class="fadeIn first">
-                    <img src="http://placehold.it/20x20" id="icon" alt="User Icon" />
+                <div class="form-floating">
+                    <input type="email" value={username} onChange={e => setusername(e.target.value)} class="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" value={password} onChange={e => setpassword(e.target.value)} class="form-control" id="floatingPassword" placeholder="Password" />
+                    <label for="floatingPassword">Password</label>
                 </div>
 
-                <form>
-                    <input type="text" id="login" class="fadeIn second" name="login" placeholder="login" />
-                    <input type="text" id="password" class="fadeIn third" name="login" placeholder="password" />
-                    <input type="submit" class="fadeIn fourth" value="Log In" />
-                </form>
-
-                <div id="formFooter">
-                    <a class="underlineHover" href="#">Forgot Password?</a>
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me" /> Remember me
+                    </label>
                 </div>
-
-            </div>
-        </div>
+                <input class="w-100 btn btn-lg btn-primary" type="submit" value="Sign in" />
+                <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+            </form>
+        </main>
 
     )
 }
--- src/Register.js
+++ src/Register.js
@@ -1,33 +1,54 @@
-import React from 'react'
-import './Register.css';
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
+// import './Register.css';
+import env from "./settings";
 
 function Register() {
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+    const [confirmpassword, setconfirmpassword] = useState("");
+    const history = useHistory()
+    let handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log({ username, password, confirmpassword })
+        try {
+            await axios.post(`${env.api}/register`, { username, password });
+            history.push("/login")
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
-    
-           <form>
-  <label>
-    <p class="label-txt">ENTER YOUR EMAIL</p>
-    <input type="text" class="input"/>
-    <div class="line-box">
-      <div class="line"></div>
-    </div>
-  </label>
-  <label>
-    <p class="label-txt">ENTER YOUR NAME</p>
-    <input type="text" class="input"/>
-    <div class="line-box">
-      <div class="line"></div>
-    </div>
-  </label>
-  <label>
-    <p class="label-txt">ENTER YOUR PASSWORD</p>
-    <input type="text" class="input"/>
-    <div class="line-box">
-      <div class="line"></div>
-    </div>
-  </label>
-  <button type="submit">submit</button>
-</form>
+        <main class="form-signin text-center">
+            <form onSubmit={(e) => {
+                handleSubmit(e)
+            }}>
+                <img class="mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
+                <h1 class="h3 mb-3 fw-normal">Please Register</h1>
+
+                <div class="form-floating">
+                    <input type="email" value={username} onChange={e => setusername(e.target.value)} class="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" value={password} onChange={e => setpassword(e.target.value)} class="form-control" id="floatingPassword" placeholder="Password" />
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" value={confirmpassword} onChange={e => setconfirmpassword(e.target.value)} class="form-control" id="floatingPassword" placeholder="Password" />
+                    <label for="floatingPassword">Confirm Password</label>
+                </div>
+
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me" /> Remember me
+                    </label>
+                </div>
+                <input class="w-100 btn btn-lg btn-primary" type="submit" value="Sign up" />
+                <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+            </form>
+        </main>
     )
 }
 
